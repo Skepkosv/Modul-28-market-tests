973da968b5b2adb8022a64820873aba45ae1e22c
Святослав <87547957+Skepkosv@users.noreply.github.com>
2022-01-17 18:50:29 +0300

    Update tests_yanmark.py

--- tests/tests_yanmark.py
+++ tests/tests_yanmark.py
@@ -7,87 +7,87 @@ from pages.yandmarket import MainPage
 def test_open_catalog(web_browser):
     page = MainPage(web_browser)
     page.find_run_button.click()
-    assert page.open_new_page == page.open_catalog
+    assert page.find_run_button
 
 def test_open_scores(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_1.click()
-    assert page.open_new_page == page.open_scores
+    assert page.find_run_btn_1
 
 def test_open_orders(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_2.click()
-    assert page.open_new_page == page.open_orders
+    assert page.find_run_btn_2
 
 def test_open_favorites(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_3.click()
-    assert page.open_new_page == page.open_favorites
+    assert page.find_run_btn_3
 
 def test_open_basket(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_4.click()
-    assert page.open_new_page == page.open_basket
+    assert page.find_run_btn_4
 
 def test_open_Moscow(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_5.click()
-    assert page.open_new_page == page.open_map
+    assert page.find_run_btn_5
 
 def test_open_new_year2022(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_6.click()
-    assert page.open_new_page == page.open_new_year2022
+    assert page.find_run_btn_6
 
 def test_open_express(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_7.click()
-    assert page.open_new_page == page.open_express_page
+    assert page.find_run_btn_7
 
 def test_open_odezda(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_8.click()
-    assert page.open_new_page == page.open_odezda
+    assert page.find_run_btn_8
 
 def test_open_tovary_dlia_doma(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_9.click()
-    assert page.open_new_page == page.open_tovary_dlia_doma
+    assert page.find_run_btn_9
 
 def test_open_elektronika(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_10.click()
-    assert page.open_new_page == page.open_elektronika
+    assert page.find_run_btn_10
 
 def test_open_bytovaia_tekhnika(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_11.click()
-    assert page.open_new_page == page.open_bytovaia_tekhnika
+    assert page.find_run_btn_11
 
 def test_open_tovary_dlia_doma1(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_9.click()
-    assert page.open_new_page == page.open_tovary_dlia_doma
+    assert page.find_run_btn_9
 
 def test_open_children(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_12.click()
-    assert page.open_new_page == page.open_children
+    assert page.find_run_btn_12
 
 def test_open_sport(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_13.click()
-    assert page.open_new_page == page.open_sport
+    assert page.find_run_btn_13
 
 def test_open_broadcasts(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_14.click()
-    assert page.open_new_page == page.open_broadcasts-page
+    assert page.find_run_btn_14
 
 def test_open_sell_on_the_market(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_15.click()
-    assert page.open_new_page == page.open_sell_on_the_market
+    assert page.find_run_btn_15
 
 def test_check_main_search_rus(web_browser):
     """ Make sure main search works fine. """
@@ -336,91 +336,96 @@ def test_main_search_natural_spruse2(web_browser):
 
 def test_open_odezhda_obuv_i_aksessuary(web_browser):
     page = MainPage(web_browser)
+    page.find_run_btn.click()
     page.find_run_btn_16.click()
-    assert page.open_new_page == page.open_odezhda_obuv_i_aksessuary
+    assert page.find_run_btn_16
 
 def test_open_tovary_dlia_sporta_i_otdykha(web_browser):
     page = MainPage(web_browser)
+    page.find_run_btn.click()
     page.find_run_btn_17.click()
-    assert page.open_new_page == page.open_tovary_dlia_sporta_i_otdykha
+    assert page.find_run_btn_17
 
 def test_open_detskie_tovary(web_browser):
     page = MainPage(web_browser)
+    page.find_run_btn.click()
     page.find_run_btn_18.click()
-    assert page.open_new_page == page.open_detskie_tovary
+    assert page.find_run_btn_18
 
 def test_open_elektroniks(web_browser):
     page = MainPage(web_browser)
+    page.find_run_btn.click()
     page.find_run_btn_19.click()
-    assert page.open_new_page == page.open_elektronika
+    assert page.find_run_btn_19
 
 def test_open_kompiuternaia_tekhnika(web_browser):
     page = MainPage(web_browser)
+    page.find_run_btn.click()
     page.find_run_btn_20.click()
-    assert page.open_new_page == page.open_kompiuternaia_tekhnika
+    assert page.find_run_btn_20
 
 def test_open_choice_goods(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_21.click()
-    assert page.open_new_page == page.open_choice_goods
+    assert page.find_run_btn_21
 
 def test_open_conditions(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_22.click()
-    assert page.open_new_page == page.open_conditions
+    assert page.find_run_btn_22
 
 def test_open_troubleshooting(web_browser):
     page = MainPage(web_browser)
-    page.find.element = 'обратная связь'
-    assert page.open_new_page == page.open_troubleshooting
+    page.find_run_btn_23.click()
+    assert page.find_run_btn_23
 
 def test_open_about(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_24.click()
-    assert page.open_new_page == page.open_about
+    assert page.find_run_btn_24
 
 def test_open_jobs_pages_usability(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_25.click()
-    assert page.open_new_page == page.open_jobs_pages_usability
+    assert page.find_run_btn_25
 
 def test_open_return(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_26.click()
-    assert page.open_new_page == page.open_return
+    assert page.find_run_btn_26
 
 def test_open_partner(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_27.click()
-    assert page.open_new_page == page.open_partner
+    assert page.find_run_btn_27
 
 def test_open_blog(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_28.click()
-    assert page.open_new_page == page.open_blog
+    assert page.find_run_btn_28
 
 def test_open_marketaff(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_29.click()
-    assert page.open_new_page == page.open_marketaff
+    assert page.find_run_btn_29
 
 def test_open_yandex_market_in_facebook(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_30.click()
-    assert page.open_new_page == page.open_yandex_market_in_facebook
+    assert page.find_run_btn_30
 
 def test_open_yandex_market_in_vk(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_31.click()
-    assert page.open_new_page == page.open_yandex_market_in_vk
+    assert page.find_run_btn_31
 
 def test_open_yandex_market_in_instagram(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_32.click()
-    assert page.open_new_page == page.open_yandex_market_in_instagram
+    assert page.find_run_btn_32
 
 def test_open_auth(web_browser):
     page = MainPage(web_browser)
     page.find_run_btn_33.click()
-    assert page.open_new_page == page.open_auth
+    assert page.find_run_btn_33
 
